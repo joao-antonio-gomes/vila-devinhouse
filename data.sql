40616779df6ff69e8dad77e94ca9cb38d8ba5930
João Gomes <joaoantoniog96@gmail.com>
2022-01-02 13:47:44 -0300

    Atualizado data.sql

--- data.sql
+++ data.sql
@@ -1,9 +1,8 @@
-create schema if not exists `village_manager`;
+create schema if not exists village_manager;
 
-create table if not exists villagers
+create table village_manager.villagers
 (
-    vlgr_id         serial
-        primary key,
+    vlgr_id serial primary key,
     vlgr_first_name varchar(255) not null,
     vlgr_surname    varchar(255) not null,
     vlgr_cpf        varchar(14)  not null,
@@ -14,20 +13,22 @@ create table if not exists villagers
     vlgr_email      varchar(255) not null
 );
 
-create table if not exists financial_report
+create unique index villagers_vlgr_cpf_uindex
+    on village_manager.villagers (vlgr_cpf);
+
+create table village_manager.financial_report
 (
-    fr_id                         serial
-        primary key,
-    fr_date                       date default CURRENT_DATE not null,
-    fr_remaining_budget           numeric                   not null,
-    fr_total_expenses             numeric                   not null,
-    fr_total_budget               numeric                   not null,
-    fr_most_expensive_villager_id integer                   not null
-        references villagers
+    fr_id serial primary key,
+    fr_date                       date    not null,
+    fr_remaining_budget           numeric not null,
+    fr_total_expenses             numeric not null,
+    fr_total_budget               numeric not null,
+    fr_most_expensive_villager_id integer not null
+        references village_manager.villagers
 );
 
-INSERT INTO village_manager.villagers (vlgr_first_name, vlgr_surname, vlgr_cpf, vlgr_birth_date, vlgr_rent,
-                                       vlgr_password, vlgr_role, vlgr_email)
-VALUES ('admin', 'admin', '000.000.000-00', '2021-12-18', 5000,
+insert into village_manager.villagers (vlgr_first_name, vlgr_surname, vlgr_cpf, vlgr_birth_date, vlgr_rent,
+                                       vlgr_password,
+                                       vlgr_role, vlgr_email)
+values ('admin', 'admin', '000.000.000-00', '2001-11-18', 5000,
         '$2a$12$gDbjHbSDg6lHHaTztxa/IuVL1SVzRFQz2bm1JUyFG0ma/ZsZWiGmS', '[ROLE_ADMIN]', 'admin@email.com');
-
